27581347a3d701fbf3e86e98722cde27e05f21b8
vitthalnamdev <122560236+vitthalnamdev@users.noreply.github.com>
2024-03-29 19:55:19 +0530

    Add files via upload

---
+++ multiplicative_inverse.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// modulo inverse .i.e =>  (a/b)%P = (a*(X))%P , where X is the multiplicative inverse of b or ((b*X)%P is =>1.)
+	// We can't calculate X for every number, 
+}
